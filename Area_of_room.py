9d0b331e19658d2d9eba81cde9c0cf5c05160e70
sowmya-saini <20wh1a0513@bvrithyderabad.edu.in>
2022-12-02 22:12:27 +0530

    Create Area_of_room.py
    Area of a room

---
+++ Area_of_room.py
@@ -0,0 +1,8 @@
+def Area_of_room():
+    width = float(input("Enter the width: "))
+    length = float(input("Enter the length: "))
+    print("Width is", width, "m")
+    print("Length is", length, "m")
+    area = width * length
+    print("The area of the room is",area, "m*m")
+Area_of_room()
