14ce246dbafdcbaea7b0b202c5b1aafc1eaf0d42
sowmya-saini <93433599+sowmya-saini@users.noreply.github.com>
2022-12-03 15:39:36 +0530

    Delete area_volume.py

--- area_volume.py
+++
@@ -1,8 +0,0 @@
-import math
-def area_volume():
-    radius = float(input("Enter the radius is:"))
-    area_circle = math.pi * radius * radius
-    volume_sphere = (4/3) * math.pi * radius * radius * radius
-    print("Area of circle is:", area_circle)
-    print("Volume of sphere is:", volume_sphere)
-area_volume()
