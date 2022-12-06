612191170ae46da53c40901e1f184467e64ccde4
sowmya-saini <20wh1a0513@bvrithyderabad.edu.in>
2022-12-06 15:01:10 +0530

    even or odd

---
+++ decision making/even_or_odd.py
@@ -0,0 +1,7 @@
+def even_odd():
+    number = int(input())
+    if number % 2 == 0:
+        print("The number", number , "is even")
+    else:
+        print("The number", number, "is odd")
+even_odd()
