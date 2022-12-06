ab325e968ac046cf252cac10928b0f5bf323f767
sowmya-saini <20wh1a0513@bvrithyderabad.edu.in>
2022-12-06 15:31:27 +0530

    season month

---
+++ decision making/season_from_day_month.py
@@ -0,0 +1,13 @@
+def season_day_month():
+    month = input()
+    day = int(input())
+    if month == "March" and day == 20:
+        print("Spring")
+    elif  month == "June" and day == 21:
+        print("Summer")
+    elif month == "September" and day == 22:
+        print("Fall")
+    elif  month == "March" and day == 20:
+        print("Winter")
+season_day_month()
+        
