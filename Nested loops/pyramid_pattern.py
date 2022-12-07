e02e668b714a76e329f043bed679e1ec0c2a562e
sowmya-saini <20wh1a0513@bvrithyderabad.edu.in>
2022-12-07 15:03:43 +0530

    pyramid pattern

---
+++ Nested loops/pyramid_pattern.py
@@ -0,0 +1,9 @@
+def pyramid():
+    n = int(input())
+    for i in range(n):
+        for s in range(n-i-1):
+            print(" ", end = " ")
+        for j in range(2 * i +1):
+            print("*", end = " ")
+        print(" ")
+pyramid()
