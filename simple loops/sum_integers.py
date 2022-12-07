0b10925dccae7a607616394b19dca43f24126c6a
sowmya-saini <20wh1a0513@bvrithyderabad.edu.in>
2022-12-07 15:09:10 +0530

    sum of integers

---
+++ simple loops/sum_integers.py
@@ -0,0 +1,8 @@
+def sum_of_integers():
+    number = int(input())
+    sum_all = 0
+    for i in range(1, number+1):
+        sum_all += i
+    print("sum of 1st n integers is", sum_all)
+sum_of_integers()
+
