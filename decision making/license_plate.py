e29fb324973a0113c9b6b5a74c47a08cb58c0507
sowmya-saini <20wh1a0513@bvrithyderabad.edu.in>
2022-12-07 15:03:52 +0530

    Update license_plate.py

--- decision making/license_plate.py
+++ decision making/license_plate.py
@@ -4,14 +4,14 @@ def old_new_license():
     if length == 6:
         if platenum[0].isupper() and  platenum[1].isupper() and platenum[2].isupper() and platenum[3].isdigit() and platenum[4].isdigit() and platenum[5].isdigit():
             print("OLD version of license plate")
-        else:
-            print("Not valid version")
+        # else:
+        #     print("Not valid version")
     elif length == 7:
         if platenum[0].isdigit() and  platenum[1].isdigit() and platenum[2].isdigit() and platenum[3].isdigit() and platenum[4].isupper() and platenum[5].isupper() and platenum[6].isupper():
 
             print("New version of license plate")
-        else:
-            print("Not valid version")
+        # else:
+        #     print("Not valid version")
     else:
         print("Not valid version")
 old_new_license()
