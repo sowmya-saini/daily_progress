db304b3c7ee78e8fb4a5cbfb02740e5b885e1ef1
sowmya-saini <20wh1a0513@bvrithyderabad.edu.in>
2022-12-09 15:53:47 +0530

    Update sum_odd.py

--- simple loops/sum_odd.py
+++ simple loops/sum_odd.py
@@ -1,8 +1,16 @@
 def sum_odd():
     num = int(input())
     sum_odd_num = 0
-    for i in range(1, num +1):
-        if i % 2:
-            sum_odd_num += i
+    num_digit = 1
+    i = 0
+    while(i != num):
+        print(num_digit)
+        sum_odd_num += num_digit
+        num_digit += 2
+        i += 1
     print(sum_odd_num)
+    # for i in range(1, num +1):
+    #     if i % 2:
+    #         sum_odd_num += i
+    # print(sum_odd_num)
 sum_odd()
