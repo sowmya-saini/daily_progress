b30c95c938a61bd965f16819e275309087f73cad
sowmya-saini <20wh1a0513@bvrithyderabad.edu.in>
2022-12-12 16:26:27 +0530

    is prime

---
+++ functions1/is_prime.py
@@ -0,0 +1,15 @@
+def is_prime(number):
+    count = 0
+    for i in range(1, number+1):
+        if number % i == 0:
+            count += 1
+       
+    if count == 2:
+        return "It is a prime number"
+    else:
+        return "It is not a prime number"
+print(is_prime(1))
+print(is_prime(2))
+print(is_prime(3))
+print(is_prime(5))
+print(is_prime(762))
