009ab90a47604ad07ddcdeab30e256639b2b5958
sowmya-saini <20wh1a0513@bvrithyderabad.edu.in>
2022-12-12 14:35:56 +0530

    caesar cipher

--- simple loops/caesar_cipher.py
+++ simple loops/caesar_cipher.py
@@ -3,10 +3,16 @@ def caesar_cipher():
     for i in range(len(string_input)):
         if string_input[i] == "X":
             char = 65
+        elif string_input[i]=="x":
+            char = 97
         elif string_input[i] == "Y":
             char = 66
+        elif string_input[i]=="y":
+            char = 98
         elif string_input[i] == "Z":
             char = 67
+        elif string_input[i]=="z":
+            char = 99
         else:
             char = ord(string_input[i]) + 3
         print(chr(char), end = "")
