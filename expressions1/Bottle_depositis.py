4dfc008313582f824595fe927809502501400af7
sowmya-saini <20wh1a0513@bvrithyderabad.edu.in>
2022-12-13 19:37:25 +0530

    bottle deposits

--- expressions1/Bottle_depositis.py
+++ expressions1/Bottle_depositis.py
@@ -2,5 +2,5 @@ def bottle_deposits():
     onel_or_less_container = int(input())
     more_than_onel_container = int(input())
     refund = onel_or_less_container*0.10 + more_than_onel_container*0.25
-    print("$", round(refund, 2))
+    print("$", format(refund, ".2f"))
 bottle_deposits()
