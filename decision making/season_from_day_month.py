99438d9c25272e03a50e711b05a24299d9e8c405
sowmya-saini <20wh1a0513@bvrithyderabad.edu.in>
2022-12-13 21:41:14 +0530

    season from day month

--- decision making/season_from_day_month.py
+++ decision making/season_from_day_month.py
@@ -1,13 +1,14 @@
 def season_day_month():
     month = input()
     day = int(input())
-    if month == "March" and day == 20:
+    if (month == "March" and day >= 20) or month == "April" or month == "May" or (month == "June" and day < 21):
         print("Spring")
-    elif  month == "June" and day == 21:
+    elif (month == "June" and day >= 21) or month == "July" or month == "August" or (month == "September" and day < 22):
         print("Summer")
-    elif month == "September" and day == 22:
+    elif (month == "September" and day >= 22) or month == "October" or month == "November" or (month == "December" and day < 21):
         print("Fall")
-    elif  month == "March" and day == 20:
+    elif(month == "December" and day >= 21) or month == "January" or month == "February" or (month == "March" and day < 20):
         print("Winter")
+        
 season_day_month()
         
