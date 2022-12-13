fc7c62f2cdf50e2ad046087ba6f8469508f155d3
sowmya-saini <20wh1a0513@bvrithyderabad.edu.in>
2022-12-13 20:08:42 +0530

    sum of digits

--- expressions1/sum_of_digits.py
+++ expressions1/sum_of_digits.py
@@ -1,6 +1,26 @@
 def sum_of_digits():
     num = int(input("Enter the integer: "))
-    str_num = list(str(num))
-    digits = [int(i) for i in str_num]
-    print(sum(digits))
+    # str_num = list(str(num))
+    # digits = [int(i) for i in str_num]
+    # print(sum(digits))
+
+    sum_all_digits = 0
+    reminder = num % 10
+    num = num // 10
+    sum_all_digits += reminder
+
+    reminder = num % 10
+    num = num // 10
+    sum_all_digits += reminder
+
+    reminder = num % 10
+    num = num // 10
+    sum_all_digits += reminder
+
+    reminder = num % 10
+    num = num // 10
+    sum_all_digits += reminder
+
+    print("sum of all 4 digits :",sum_all_digits)
+
 sum_of_digits()
