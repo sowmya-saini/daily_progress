b7eaeb0663d6cc1e4904f77c7f774910191c6803
sowmya-saini <20wh1a0513@bvrithyderabad.edu.in>
2022-12-14 15:16:10 +0530

    check password

---
+++ functions1/check_password.py
@@ -0,0 +1,21 @@
+def CheckPassword():
+    password = input()
+    length_password = len(password)
+    number = "no number"
+    uppercase = "no uppercase"
+    lowercase = "no lowercase"
+    if length_password == 8:
+        for i in password:
+            if i.isdigit():
+                number = "number found"
+            elif i.islower():
+                lowercase = "lowercase found"
+            elif i.isupper():
+                uppercase = "uppercase found"
+    else:
+        return False 
+    if number == "number found" and uppercase == "uppercase found" and lowercase == "lowercase found":
+        return True
+    else:
+        return False
+print(CheckPassword())
