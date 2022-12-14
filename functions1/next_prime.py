a1f55bff2ad29a3e55fc5a10b2fb87aad77b38e9
sowmya-saini <20wh1a0513@bvrithyderabad.edu.in>
2022-12-14 15:58:21 +0530

    Next prime

---
+++ functions1/next_prime.py
@@ -0,0 +1,25 @@
+def primecheck(number):
+    count = 0
+    for i in range(1, number+1):
+        if number % i == 0:
+            count += 1
+       
+    if count == 2:
+        return True
+    else:
+        return False
+
+def NextPrime(number1):
+    given_num = number1 + 1
+    greater_number = 0
+    while(greater_number != 1):
+        if primecheck(given_num) == True:
+            greater_number = 1
+            break
+        given_num += 1
+    if greater_number == 1:
+        return given_num
+
+
+print(NextPrime(7))
+
