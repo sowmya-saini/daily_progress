d93d16f9949bdbd0b238267f0d3eb01c2bcf6dbd
sowmya-saini <20wh1a0513@bvrithyderabad.edu.in>
2022-12-16 16:04:16 +0530

    approximate pi

--- functions2/approximate_pi.py
+++ functions2/approximate_pi.py
@@ -1,16 +1,12 @@
 def approximatepi():
     no_of_approximations = int(input())
     val_approximate = 0
-    if no_of_approximations == 1:
-        val_approximate = 3
-        return val_approximate
-    else:
-        for i in range(no_of_approximations+1):
-            if  i == 0:
-                val_approximate = 3
-            elif i % 2 != 0:
-                val_approximate += 4/((2*i) * (2*i + 1) * (2*i+2))
-            else:
-                val_approximate -= 4/((2*i) * (2*i + 1) * (2*i+2))
-        return val_approximate
+    for i in range(no_of_approximations+1):
+        if  i == 0:
+            val_approximate = 3
+        elif i % 2 != 0:
+            val_approximate += 4/((2*i) * (2*i + 1) * (2*i+2))
+        else:
+            val_approximate -= 4/((2*i) * (2*i + 1) * (2*i+2))
+    return val_approximate
 print(approximatepi())
