338c3bf696225fb845dc76b97ef73f1da53c3e9c
sowmya-saini <20wh1a0513@bvrithyderabad.edu.in>
2022-12-19 22:01:37 +0530

    Mystery length

--- Strings/MyteryLength.py
+++ Strings/MyteryLength.py
@@ -1,14 +1,14 @@
-def removeMysteryLength(string_input, length):
-    string_res = ""
-    i = 0
-    while(len(string_res) != length):
-        string_res += string_input[i]
-        i+=1
-    return string_res
-print(removeMysteryLength("sainiSowmya25", 10))
-
-#method2
-
-def removeMysteryLength(string_input, length):
-    return string_input[:length]
-print(removeMysteryLength("sainiSowmya25", 10))
+def removeMysteryLength(string_input):
+    length = len(string_input)
+    number = ""
+    i=0
+    num = 0
+    while(num < length):
+        number = string_input[length - i - 1:] 
+        num = int(number)
+        i += 1
+    l = string_input[length-len(number) + 1:]
+    return string_input[:int(l)]
+print(removeMysteryLength("sainiSowmya25"))
+print(removeMysteryLength("JamesBond00712"))
+print(removeMysteryLength("sjhdkjkjkdsksdkjsdl1jhh122"))
