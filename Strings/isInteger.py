d05fc50d5044e0fcd753e407d8683335bcc18f3a
sowmya-saini <20wh1a0513@bvrithyderabad.edu.in>
2022-12-23 14:58:49 +0530

    is Integer

--- Strings/isInteger.py
+++ Strings/isInteger.py
@@ -1,21 +1,25 @@
 def isInteger(string_input):
     without_spaces = string_input.replace(' ', '')
     print(without_spaces)
-    validInteger = 0
-    for i in range(len(without_spaces)):
-        if i==0 and (without_spaces[i] == '+' or without_spaces[i] == '-'):
-            validInteger = 0
-        elif without_spaces[i].isdigit():
-            validInteger = 0
-        else:
-            validInteger = 1
-            break
-    if validInteger == 0:
-        return "Given string represents valid Integer"
-    else:
+    if len(without_spaces) == 1 and (without_spaces == '+' or without_spaces == "-"):
         return "Given string is not a valid Integer"
+    else:
+        validInteger = 0
+        for i in range(len(without_spaces)):
+            if i==0 and (without_spaces[i] == '+' or without_spaces[i] == '-'):
+                validInteger = 0
+            elif without_spaces[i].isdigit():
+                validInteger = 0
+            else:
+                validInteger = 1
+                break
+        if validInteger == 0:
+            return "Given string represents valid Integer"
+        else:
+            return "Given string is not a valid Integer"
 
-
+print(isInteger("  +"))
+print(isInteger("  +1"))
 print(isInteger(" 1 2 + 4 5"))
 print(isInteger(" + 1 2 + 4 5"))
 print(isInteger(" - +  2 4 5"))
