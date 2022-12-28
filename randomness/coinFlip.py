633a914de3d0870b6a2cad4f4584ced3a3f2545d
sowmya-saini <20wh1a0513@bvrithyderabad.edu.in>
2022-12-28 06:55:46 +0530

    Coin Flip

--- randomness/coinFlip.py
+++ randomness/coinFlip.py
@@ -9,14 +9,15 @@ def head_or_tail():
 def getNumOfFlips():
     head = ""
     tail = ""
+    terminate = 0
     no_of_flips = 0
-    for i in range(10):
+    while(terminate != 1):
         num = head_or_tail()
-        print(num)
+        # print(num)
         no_of_flips += 1
-        print("no of flips",no_of_flips)
+        # print("no of flips",no_of_flips)
         if (head == "Head" and num == 0) or (tail == "Tail" and num == 1):
-            break
+            terminate = 1
         if num == 0:
             head = "Head"
             tail = ""
@@ -26,7 +27,20 @@ def getNumOfFlips():
     return no_of_flips
 getNumOfFlips()
 
-# def resultant_flips():
-#     no_of_flips = getNumOfFlips()
-#     return no_of_flips
-# print(resultant_flips())
+def resultant_flips():
+    minimum = 0
+    maximum = 0
+    Average = 0
+    for i in range(10):
+        no_of_flips = getNumOfFlips()
+        Average += no_of_flips
+        print(no_of_flips)
+        if minimum == 0 or minimum > no_of_flips:
+            minimum = no_of_flips
+        elif maximum == 0 or maximum <no_of_flips:
+            maximum = no_of_flips
+    print("Minimum number of flips :",minimum)
+    print("Maximum number of flips :", maximum)
+    print("Average number of flips :",Average//10)
+   
+resultant_flips()
